4607be9ad13efc4e0cc6a7b0069bc51cabe90d7d
asadnaseerr <asadnaseer7675@yahoo.com>
2025-10-03 00:01:42 +0300

    Task 4 Updated

--- Task 4/task4_code.py
+++ Task 4/task4_code.py
@@ -1,8 +1,8 @@
 # Draw a square or rectangle around a specific part of the image with a yellow border. Fill the inside of this area.
 import cv2
 img = cv2.imread('Data/IMG_5378.JPG')
-top_left = (150, 100)
-bottom_right = (450, 400)
+top_left = (800, 1000)
+bottom_right = (1500, 1800)
 cv2.rectangle(img, top_left, bottom_right, (0, 255, 255), thickness=-1)
 cv2.rectangle(img, top_left, bottom_right, (0, 255, 255), thickness=5)
 cv2.imwrite('Data/rectangle_filled_image.JPG', img)
